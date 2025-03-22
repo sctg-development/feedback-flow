b29312a8778636b798bd2179e7938a70e88dc087
æLtorio <aeltorio@universidadatlantico.org>
2025-03-22 15:31:03 +0100

    Update site configuration with new links and improved structure

--- client/src/config/site.ts
+++ client/src/config/site.ts
@@ -1,61 +1,64 @@
 export type SiteConfig = typeof siteConfig;
 import i18next from "../i18n";
 
-export const siteConfig = () => ({
-  name: i18next.t("vite-heroui"),
-  description: i18next.t(
-    "make-beautiful-websites-regardless-of-your-design-experience",
-  ),
-  navItems: [
-    {
-      label: i18next.t("home"),
-      href: "/",
+export const siteConfig = () => {
+  const t = i18next.t;
+
+  return {
+    name: t("Feedback Flow"),
+    description: i18next.t(
+      "make-beautiful-websites-regardless-of-your-design-experience",
+    ),
+    navItems: [
+      {
+        label: i18next.t("home"),
+        href: "/",
+      },
+      {
+        label: i18next.t("api"),
+        href: "/api",
+      },
+      {
+        label: i18next.t("pricing"),
+        href: "/pricing",
+      },
+      {
+        label: i18next.t("blog"),
+        href: "/blog",
+      },
+      {
+        label: i18next.t("about"),
+        href: "/about",
+      },
+    ],
+    navMenuItems: [
+      {
+        label: i18next.t("home"),
+        href: "/",
+      },
+      {
+        label: i18next.t("api"),
+        href: "/api",
+      },
+      {
+        label: i18next.t("pricing"),
+        href: "/pricing",
+      },
+      {
+        label: i18next.t("blog"),
+        href: "/blog",
+      },
+      {
+        label: i18next.t("about"),
+        href: "/about",
+      },
+    ],
+    links: {
+      github: "https://github.com/sctg-development/feedback-flow",
+      sctg: "https://sctg.eu.org",
+      docs: "https://github.com/sctg-development/feedback-flow/blob/main/README.md",
+      // discord: "https://discord.gg/9b6yyZKmH4",
+      sponsor: "https://github.com/sponsors/sctg-development",
     },
-    {
-      label: i18next.t("api"),
-      href: "/api",
-    },
-    {
-      label: i18next.t("pricing"),
-      href: "/pricing",
-    },
-    {
-      label: i18next.t("blog"),
-      href: "/blog",
-    },
-    {
-      label: i18next.t("about"),
-      href: "/about",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: i18next.t("home"),
-      href: "/",
-    },
-    {
-      label: i18next.t("api"),
-      href: "/api",
-    },
-    {
-      label: i18next.t("pricing"),
-      href: "/pricing",
-    },
-    {
-      label: i18next.t("blog"),
-      href: "/blog",
-    },
-    {
-      label: i18next.t("about"),
-      href: "/about",
-    },
-  ],
-  links: {
-    github:
-      "https://github.com/sctg-development/vite-react-heroui-auth0-template",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://github.com/sctg-development/vite-react-heroui-auth0-template/blob/main/README.md",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://github.com/sponsors/sctg-development",
-  },
-});
+  };
+};
