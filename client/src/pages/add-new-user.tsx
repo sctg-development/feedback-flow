73ffd7221011f0a3dda8117d0724b744e35a4fd2
æLtorio <aeltorio@universidadatlantico.org>
2025-03-24 11:21:30 +0100

    wip: Enhance AddNewUser component with admin permission check and tester list refresh

--- client/src/pages/add-new-user.tsx
+++ client/src/pages/add-new-user.tsx
@@ -5,51 +5,93 @@ import { Form } from "@heroui/form";
 import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 
+import {
+  GetTestersResponse,
+  OrderCriteria,
+  Tester,
+  TesterCreateResponse,
+  TesterSortCriteria,
+} from "@/types/data";
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
-import { postJsonToSecuredApi, userHasPermission } from "@/components/auth0";
+import {
+  getJsonFromSecuredApi,
+  postJsonToSecuredApi,
+  userHasPermission,
+} from "@/components/auth0";
 
 export default function AddNewUser() {
   const { t } = useTranslation();
   const { isAuthenticated, getAccessTokenSilently } = useAuth0();
 
-  const [hasPermission, setHasPermission] = useState(false);
-  const [apiResponse, setApiResponse] = useState(
-    null as {
-      [k: string]: FormDataEntryValue;
-    } | null,
-  );
+  const [hasAdminPermission, setHasAdminPermission] = useState(false);
+  const [testers, setTesters] = useState([] as Array<Tester>);
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [sort, setSort] = useState("name" as TesterSortCriteria);
+  const [order, setOrder] = useState("asc" as OrderCriteria);
+  /**
+   * Function to refresh the list of testers
+   */
+  const refreshTesters = async (
+    page: number,
+    limit: number,
+    sort: TesterSortCriteria,
+    order: OrderCriteria,
+  ) => {
+    const response = (await getJsonFromSecuredApi(
+      `${import.meta.env.API_BASE_URL}/testers?page=${page}&limit=${limit}&sort=${sort}&order=${order}`,
+      getAccessTokenSilently,
+    )) as GetTestersResponse;
+
+    if (response && response.success) {
+      setTesters(response.data);
+    }
+  };
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.currentTarget));
+
     const ids = [data["oauthId"] as string];
     const name = data["name"] as string;
 
-    const apiResponse = await postJsonToSecuredApi(
+    const apiResponse = (await postJsonToSecuredApi(
       `${import.meta.env.API_BASE_URL}/tester`,
       { name, ids },
       getAccessTokenSilently,
-    );
+    )) as TesterCreateResponse;
 
-    setApiResponse(apiResponse);
+    if (apiResponse && apiResponse.success) {
+      console.log(`Created tester with UUID: ${apiResponse.uuid}`);
+
+      // Refresh the list of testers
+      await refreshTesters(page, limit, sort, order);
+    }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       if (isAuthenticated) {
         try {
-          setHasPermission(
+          setHasAdminPermission(
             await userHasPermission(
               import.meta.env.ADMIN_PERMISSION,
               getAccessTokenSilently,
             ),
           );
+          if (hasAdminPermission) {
+            await refreshTesters(page, limit, sort, order);
+          }
+
+          // eslint-disable-next-line no-console
           console.log(
-            `You have ${hasPermission ? "" : "not "}permission to add a new user.`,
+            `You have ${hasAdminPermission ? "" : "not "}permission to add a new user.`,
           );
         } catch (error) {
+          // eslint-disable-next-line no-console
           console.log(error as Error);
         }
       }
@@ -58,14 +100,21 @@ export default function AddNewUser() {
     fetchData();
   }, [isAuthenticated]);
 
+  useEffect(() => {
+    if (isAuthenticated && hasAdminPermission) {
+      refreshTesters(page, limit, sort, order);
+    }
+  }, [page, limit, sort, order, isAuthenticated, hasAdminPermission]);
+
   return (
     <DefaultLayout>
-      {hasPermission ? (
+      {hasAdminPermission ? (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
           <div className="inline-block max-w-lg text-center justify-center">
             <h1 className={title()}>
               <Trans t={t}>add-a-new-user</Trans>
             </h1>
+            <pre>{JSON.stringify(testers, null, 2)}</pre>
           </div>
           <Form className="w-full max-w-xs" onSubmit={onSubmit}>
             <Input
@@ -90,11 +139,6 @@ export default function AddNewUser() {
             <Button type="submit" variant="bordered">
               {t("submit")}
             </Button>
-            {apiResponse && (
-              <div className="text-small text-default-500">
-                {t('api-response')}: <code>{JSON.stringify(apiResponse)}</code>
-              </div>
-            )}
           </Form>
         </section>
       ) : (
