87fe717c8301ac5c9c95200f6871622f1f01082d
æLtorio <aeltorio@universidadatlantico.org>
2025-03-28 18:27:08 +0100

    wip: purchases page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,64 +1,45 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import PaginatedTable from "@/components/paginated-table";
 
 export default function IndexPage() {
   const { t } = useTranslation();
+  const { isAuthenticated } = useAuth0();
 
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>{t("make")}&nbsp;</span>
-          <span className={title({ color: "violet" })}>
-            {t("beautiful")}&nbsp;
-          </span>
-          <br />
-          <span className={title()}>
-            <Trans i18nKey="websites-regardless-of-your-design-experience" />
-          </span>
-          <div className={subtitle({ class: "mt-4" })}>
-            <Trans i18nKey="beautiful-fast-and-modern-react-ui-library" />
-          </div>
-        </div>
-
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig().links.docs}
-          >
-            <Trans i18nKey="documentation" />
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig().links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              <Trans i18nKey="get-started-by-editing" />{" "}
-              <Code color="primary">pages/index.tsx</Code>
+        {!isAuthenticated ? (
+          <div className="inline-block max-w-lg text-center justify-center">
+            <span className={title()}>{"Manage all your"}&nbsp;</span>
+            <span className={title({ color: "violet" })}>
+              {"feedbacks"}&nbsp;
             </span>
-          </Snippet>
-        </div>
+            <br />
+            <span className={title()}>{"in one place."}</span>
+          </div>
+        ) : (
+          <div className="flex gap-3">
+            <PaginatedTable
+              columns={[
+                { field: "id", label: t("id"), sortable: false },
+                { field: "date", label: t("date"), sortable: true },
+                { field: "order", label: t("order"), sortable: true },
+                {
+                  field: "description",
+                  label: t("description"),
+                  sortable: false,
+                },
+                { field: "amount", label: t("amount"), sortable: true },
+              ]}
+              dataUrl={`${import.meta.env.API_BASE_URL}/purchases/not-refunded`}
+              title={"Purchases not refunded"}
+            />
+          </div>
+        )}
       </section>
     </DefaultLayout>
   );
