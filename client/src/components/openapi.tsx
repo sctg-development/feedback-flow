267ff4172c1d03c3bada7d87baa5ff9f60125af7
æLtorio <aeltorio@universidadatlantico.org>
2025-04-01 17:48:18 +0200

    feat: implement dynamic fetching of OpenAPI specification and update server URLs in Swagger UI

--- client/src/components/openapi.tsx
+++ client/src/components/openapi.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import { useEffect, useState } from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
@@ -6,5 +8,25 @@ export function OpenAPI() {
     ? import.meta.env.BASE_URL + "openapi.json"
     : import.meta.env.BASE_URL + "/openapi.json";
 
-  return <SwaggerUI url={url} />;
+  // Fetch the OpenAPI spec from the server
+  const [openApiSpec, setOpenApiSpec] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.servers = [
+          {
+            url: import.meta.env.API_BASE_URL.endsWith("/api")
+              ? import.meta.env.API_BASE_URL.split("/api")[0]
+              : import.meta.env.API_BASE_URL,
+            description: "API server",
+          },
+        ];
+        setOpenApiSpec(data);
+      })
+      .catch((error) => console.error("Error fetching OpenAPI spec:", error));
+  }, [url]);
+
+  return <SwaggerUI spec={openApiSpec as unknown as Object} />;
 }
